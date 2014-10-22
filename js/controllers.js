33bde25a5908c777a00601966a0174bd9aaf4d01
hybridmobileapp <ashish.phonegap@gmail.com>
2014-10-22 18:42:37 +0530

    only root

---
+++ js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope , itunes) {
+itunes.query("hip-hop");
+
+$scope.doSearch = function (term){
+    //alert("search");
+	itunes.query(term);
+}
+
+$scope.$on('data:updated', function(event,data) {
+     // you could inspect the data to see if what you care about changed, or just update your own scope
+     $scope.items = itunes.all();
+   });
+
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
---
+++ js/services.js
@@ -0,0 +1,59 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Scruff McGruff' },
+    { id: 1, name: 'G.I. Joe' },
+    { id: 2, name: 'Miss Frizzle' },
+    { id: 3, name: 'Ash Ketchum' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+})
+
+
+.factory('itunes', function( $http , $rootScope ) {
+  var list = [];
+
+
+var term = "hipHop";
+ return {
+    query: function(search){
+              term = search;
+               $http.jsonp("http://itunes.apple.com/search", {
+                  params: {
+                      "callback": "JSON_CALLBACK",
+                      "term": term,
+                      "media": "musicVideo"
+                  }
+              }).success(function(data, status, headers, config) {
+                  console.log("is scope : " + JSON.stringify (data) );
+                  list = data.results;
+                  $rootScope.$broadcast('data:updated',data);
+              }).error(function(data, status, headers, config) {
+                  console.log("error" + data);
+              });
+
+    },
+    all: function() {
+      return list;
+    },
+    get: function() {
+     
+    }
+  }
+})
